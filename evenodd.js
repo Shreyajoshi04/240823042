33d2cd5dd6a8996deedc7ea594eaaec54c2d3438
shreyajoshi <joshishreya773@gmail.com>
2025-07-16 13:29:08 +0530

    updtaes

---
+++ evenodd.js
@@ -0,0 +1,104 @@
+// 1
+// function evenodd(a)
+// {
+//     if(a%2==0){
+//         console.log("even");
+//     }else{
+//         console.log("odd");
+//     }
+// }
+// export default evenodd;
+
+// 2
+// function sec(a,b)
+// {
+//     if(a>b){
+//         console.log("a is big");
+//     }else{
+//         console.log("b is big");
+//     }
+// }
+// export default sec;
+
+// 3
+// function third(a)
+// {
+//     if(a%4==0){
+//         console.log("leap year");
+//     }else{
+//         console.log("not leap year");
+//     }
+// }
+// export default third;
+
+// 4
+// function four(a){
+//     let sum=0;
+//         for(let i=0;i<=a;i++){
+            
+
+//             sum=sum+i;
+
+//         }
+//         console.log(sum);
+// }
+// export default four;
+
+// 5
+// function five(a)
+// {
+//     let fact=1;
+//     for(let i=1;i<a;i++){
+//         fact=fact*i;
+//     }
+//     console.log(fact);
+// }
+// export default five;
+
+// 6
+// function six(a)
+// {
+    
+//     for(let i=1;i<=10;i++){
+//        console.log(`${a} X ${i} = ${a * i}`);
+        
+//     }
+    
+    
+// }
+// export default six;
+
+
+// 7
+// function seven(a)
+// {
+//    return Number(a.toString().split('').reverse().join(''));
+
+
+    
+        
+//     }
+    
+    
+
+// export default seven;
+
+// 8
+// function eight(num)
+// {
+//   const str=num.toString();
+//   const reversed=str.split('').reverse().join('');
+//   if(str==reversed){
+//     return 'palindrome';
+//   }else
+//     {
+//         return ' not palindrome';
+
+
+//     }
+//   }
+// export default seven;
+
+
+   // 9
+  
--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-let a=7;
-if(a%2==0){
-    console.log("even");
-
-}else{
-    console.log("odd");
-
-}
+import eight from "./evenodd.js"
+console.log(eight(121));
