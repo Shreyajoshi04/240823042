edbb42a80cf070c917c70fba6012216f07cb4553
shreya joshi <joshishreya773@gmail.com>
2025-07-22 11:25:32 +0530

    update

---
+++ modindex.js
@@ -0,0 +1,29 @@
+
+// const add=require("./add.js");
+// console.log(add(11,22));
+
+//import path from "path";
+// const path= require("path");
+// console.log("Directory name:",__dirname);
+// console.log(path.basename(__dirname+"file.txt"));
+// console.log("full path:",path.resolve(__dirname,"file.txt"));
+
+// const moment=require("moment");
+// console.log("current date and time:",moment().format("DD-MM-YYYY HH:MM:SS"));
+
+// const ls=require("lodash");
+// console.log("capitalized:",ls.capitalize("hello world"));
+
+// const { isUtf8 } = require("buffer");
+// const fs=require("fs")
+// fs.writeFile("best.txt","Hello,MCA-Z2 class!",(err)=>{
+//     if(err)throw err;
+//     console.log("file has been saved!")
+
+// });
+
+// const fs= require("fs")
+// fs.readFile("best.txt","utf8",(err,data)=>{
+//     if(err)throw err;
+//     console.log("file content:",data);
+// });
